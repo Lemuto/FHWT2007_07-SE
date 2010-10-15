4e543330fec82ac4d6e55a34c2e688439bd847f9
Lemuto <lemuto.otis@googlemail.com>
2010-10-15 10:40:13 +0200

    Improvements on calculating Pi

--- src/Pi.java
+++ src/Pi.java
@@ -7,26 +7,25 @@ import java.lang.Math;
 public class Pi {
 
 	public static void main(String[] args) {
+		pi(5);
+	}
+	
+	private static void pi(int genauigkeit) {
 		double pi, pi_old = 4, factor = 1;
-		boolean positiv = false, check = true;
-		int i = 3; //Laufvariable
+		boolean check = true;
+		int i = 3, vorzeichen = 1;
 		while (check){
+			//Vorzeichenwechsel bei jedem Durchgang
+			vorzeichen *= -1;
 			//Berechnung des neuen Faktors
-			if (positiv) {
-				factor = factor + (1./i);
-				positiv = false;
-			}
-			else {
-				factor = factor - (1./i);
-				positiv = true;
-			}
-			//Runden auf 5 Nachkommastellen
-			pi = Math.round(4 * factor * 100000.) / 100000.;
+			factor += (1./i) * vorzeichen;
+			//Runden auf Nachkommastellen, Anzahl in genauigkeit definiert
+			pi = Math.round(4 * factor * Math.pow(10.,genauigkeit)) / Math.pow(10.,genauigkeit);
 			//Prüfen, ob sich der gerundete Wert seit dem letzten Durchgang geändert hat
 			if (pi == pi_old){
 				//Keine Verbesserung der Genauigkeit ==> Ausgabe + Abbruch
 				check = false;
-				System.out.println("Nach " + (i/2) + " Versuchen wurde die Zahl pi (" + pi + ") ermittelt!");
+				System.out.println("Nach " + (i/2) + " Durchläufen wurde die Zahl pi (" + pi + ") ermittelt!");
 			}
 			else {
 				pi_old = pi;
